268cc8c03484dcdaacb15e42d4cbe5293b58c5d3
Spacice <gargushak0990@gmail.com>
2023-07-05 14:01:45 +0400

    config.py was deleted for secure

--- config.py
+++
@@ -1,8 +0,0 @@
-settings = {
-    'token': 'MTEyNTA1MDI1NDM5NjQ5ODAzMA.GvG6iN.gkZjfPkuvCJyTOyNsMW81VikenwRhm_HGBw-iU',
-    'bot': 'Ведущий',
-    'id': 1125050254396498030,
-    'prefix': '/'
-}
-
-ilyusURL = 'https://sun9-63.userapi.com/impg/8W-eSxmpxmQgyAMSxOKc2SOGkVgMAmSt1KJAsQ/UPkDXjYPZSg.jpg?size=138x224&quality=96&sign=c8cf749ee9558f72334b800b3f225e45&type=album'
