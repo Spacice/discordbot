727761c52df96e9412a083a4b89d169dd53e2951
Spacice <gargushak0990@gmail.com>
2023-07-05 15:06:12 +0400

    added config.py, deleted Ñunnecessary files

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+    'token': 'YOUR_BOT_TOKEN',
+    'bot': 'Ведущий',
+    'id': 0,#bot's token
+    'prefix': '/'
+}
+
+ilyusURL = 'https://sun9-63.userapi.com/impg/8W-eSxmpxmQgyAMSxOKc2SOGkVgMAmSt1KJAsQ/UPkDXjYPZSg.jpg?size=138x224&quality=96&sign=c8cf749ee9558f72334b800b3f225e45&type=album'
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "E:\Проекты Python\Discord Bot\bot.py"
-pause
